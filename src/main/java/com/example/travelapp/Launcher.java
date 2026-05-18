d799da16eff20a07b2c84fc2ec405bee8fe76914
AnaBelenMon <anabelenmontilla1975@gmail.com>
2026-05-18 09:55:26 +0200

    Realización del controlador del login, el helloAplication y subida del launcher

---
+++ src/main/java/com/example/travelapp/Launcher.java
@@ -0,0 +1,9 @@
+package com.example.travelapp;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(HelloApplication.class, args);
+    }
+}
